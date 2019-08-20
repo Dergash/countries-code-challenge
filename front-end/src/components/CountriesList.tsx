391e53d59ce4927f1003a6abfd80ce645dde12be
Dergachev <dergachev@hotmail.com>
2019-08-21 06:39:42 +0700

    local pagination

--- front-end/src/components/CountriesList.tsx
+++ front-end/src/components/CountriesList.tsx
@@ -22,20 +22,59 @@ const List = styled.ul`
     grid-template-columns: repeat(3, 1fr);
     grid-gap: 8px;
     padding: 0px 8px 0px 8px;
-    align-self: flex-start;
 
     @media (max-width: 768px) {
         grid-template-columns: 1fr;
     }
 `
 
+const PaginationContainer = styled.div`
+    
+width: 100%;
+    align-self: flex-start;
+    display: flex;
+    flex-direction: column;
+`
+
+const PaginationControls = styled.div`
+    margin-top: 16px;
+    margin-right: 8px;
+    align-self: flex-end;
+`
+
+const Button = styled.button`
+    border: none;
+    background-color: ${props => !props.disabled ? 'rgb(15, 32, 45)' : 'rgb(30,40,50)'};
+    color: rgba(255, 255, 255, 0.4);
+    &:hover {
+        background-color: ${props => !props.disabled ? 'rgb(20, 37, 50)' : 'rgb(30,40,50)'};
+    }
+    outline: none;
+    height: 40px;
+    margin-left: 8px;
+    padding-left: 16px;
+    padding-right: 16px;
+    cursor: ${props => !props.disabled ? 'pointer' : 'default'};
+`
+
 const Spinner = styled.span`
     font-size: 22px;
 `
 
+const pageSize = 30
+
 export const CountriesList: FunctionComponent<CountriesListProps> = (props) => {
     const {loading, error, data} = useQuery<CountriesResponse>(fetchCountries)
+    const [ page, setPage ] = React.useState(0)
     const countries = data && data.countries
+    const handlePrevPage = React.useCallback(() => { setPage(page - 1)}, [page])
+    const handleNextPage = React.useCallback(() => { setPage(page + 1)}, [page])
+    const countriesPage = React.useMemo(() => {
+        return countries
+            ? countries.slice(page * pageSize, (page + 1) * pageSize)
+            : []
+    }, [countries, page])
+
     return <Container>
         { loading &&
             <Spinner>Fetching countries...</Spinner>
@@ -44,9 +83,25 @@ export const CountriesList: FunctionComponent<CountriesListProps> = (props) => {
             <span>{error}</span>
         }
         { !loading && !error && countries &&
-            <List>
-                {countries.map(item => <CountryCard key={item.code} country={item} />)}
-            </List>
+            <PaginationContainer>
+                <PaginationControls>
+                    <Button
+                        disabled={page === 0}
+                        onClick={handlePrevPage}
+                    >
+                        Previous page
+                    </Button>
+                    <Button
+                        disabled={(page + 1) * pageSize >= countries.length}
+                        onClick={handleNextPage}
+                    >
+                        Next page
+                    </Button>
+                </PaginationControls>
+                <List>
+                    {countriesPage.map(item => <CountryCard key={item.code} country={item} />)}
+                </List>
+            </PaginationContainer>
         }
     </Container>
 }
